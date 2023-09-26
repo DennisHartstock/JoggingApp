8ebc51246a24fe3a31c8a6f2386853b750769f97
DennsHartstock <dennis.hartstock@web.de>
2023-09-26 08:12:25 +0200

    format user image

--- app/src/main/java/com/commcode/joggingapp/ui/UserCard.kt
+++ app/src/main/java/com/commcode/joggingapp/ui/UserCard.kt
@@ -2,25 +2,30 @@ package com.commcode.joggingapp.ui
 
 import android.content.res.Configuration
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.commcode.joggingapp.R
 import com.commcode.joggingapp.ui.theme.JoggingAppTheme
 
 @Preview(name = "Light Mode")
@@ -36,7 +41,6 @@ fun UserCard() {
                 .fillMaxWidth()
                 .height(200.dp),
             border = BorderStroke(1.dp, color = MaterialTheme.colorScheme.onBackground)
-
         ) {
             Row(
                 modifier = Modifier
@@ -45,14 +49,16 @@ fun UserCard() {
                 horizontalArrangement = Arrangement.SpaceAround,
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                Box(
+                Image(
+                    painter = painterResource(id = (R.drawable.ic_android_red_24dp)),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
                     modifier = Modifier
+                        .clip(CircleShape)
                         .background(color = MaterialTheme.colorScheme.primary)
-                        .size(50.dp),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Text(text = "Image", color = MaterialTheme.colorScheme.onPrimary)
-                }
+                        .size(50.dp)
+                        .padding(8.dp)
+                )
                 UserStats("Name", "John")
                 UserStats("Total Distance", "100 km")
                 UserStats("Average Speed", "8 km/h")
